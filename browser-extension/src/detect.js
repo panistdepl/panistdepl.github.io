3b5071417f7dd3e87f1b3a248b11c7f639f67c2c
bouri <mounir.bouri@esprit.tn>
2018-12-03 11:21:25 +0100

    fixing bugs and merging firefox and chrome ressources

--- browser-extension/src/detect.js
+++ browser-extension/src/detect.js
@@ -2,10 +2,9 @@
 
 var firefoxVersion = '1.0.0';
 var firefoxLink    = 'download/panist-' + firefoxVersion + '-an+fx.xpi';
-var firefoxLinkAPI = 'download/panist-' + firefoxVersion + '-an+fx-api-panit.xpi';
 const docUrl = 'https://doc.istex.fr/users/usage/button/';
 
-/*
+
 if (bowser.chrome || bowser.chromium) {
   $('#addingToBrowser').text('Ajouter l\'extension à Chrome');
   $('#docUsers').attr('href', docUrl + '#chrome');
@@ -27,33 +26,27 @@ if (bowser.chrome || bowser.chromium) {
     });
   };
 } else 
-*/
+
 if (bowser.firefox) {
   if (bowser.check({ firefox: "49" })) {
-    $('#addingToBrowserAPI').text('Ajouter l\'extension à Firefox');
-    $('#addingToBrowserAPI').attr('href', firefoxLinkAPI);
     $('#addingToBrowser').text('Ajouter l\'extension à Firefox (Demo)');
     $('#docUsers').attr('href', docUrl + '#firefox');
     $('#addingToBrowser').attr('href', firefoxLink);
 
   } else {
-    $('#addingToBrowserAPI').text('Version du navigateur ancienne.');
     $('#addingToBrowser').text('Version du navigateur ancienne.');
     $('.tooltip-wrapper').tooltip({
       title: 'Votre version du navigateur est trop ancienne. Veuillez mettre votre navigateur à jour (minimum en version 49) pour pouvoir bénéficier de l\'extension.',
       placement: 'bottom'
     });
-    $('#addingToBrowserAPI').attr("disabled", true);
     $('#addingToBrowser').attr("disabled", true);
   }
 } else {
   // Not supported
-  $('#addingToBrowserAPI').text('Navigateur non supporté.');
   $('#addingToBrowser').text('Navigateur non supporté.');
   $('.tooltip-wrapper').tooltip({
     title: 'Votre navigateur n\'est pas supporté pour le moment. Seuls Chrome et Firefox sont disponibles. Versions Safari et Edge à venir.',
     placement: 'bottom'
   });
-  $('#addingToBrowserAPI').attr("disabled", true);
   $('#addingToBrowser').attr("disabled", true);
 }
